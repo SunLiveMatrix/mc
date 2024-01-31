f9e1c9eb03d0745a5afd397072d9ac1d51f63e50
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2024-01-31 18:16:01 -0300

    mc
    Codux is an all-in-one visual development environment. Whether you’re starting a new app, creating components, or editing an existing project, every visual change you make reflects instantly in the code and vice versa. To learn more about Codux, visit our website - [https://www.codux.com/](https://www.codux.com/)

---
+++ matrix/gnu/bin/mc.d
@@ -0,0 +1,92 @@
+module matrix.gnu.bin.mc;
+
+export class MCMatrix {
+    void mc(double x, double y, double z) {
+        /// <summary>
+    }
+}
+
+export class MCMatrixWithRotation {
+    void mcStates(double x, double y, double z){
+        /// <summary>
+    }
+}
+
+export class MCMatrixWithRotationLists {
+    void mcLists(int x, int y, int z) {
+       /// <summary>
+    }
+}
+
+export interface MCMatrixWithRotationEvent {
+    // private method for computing the rotation
+}
+
+export interface MCMatrixWithRotationObject {
+    // private method for computing the rotation
+}
+
+export interface MCMatrixWithRotationStates {
+   // private method for computing the rotation
+}
+
+
+public void matrixPlusRotations(int values) @property {
+       /// <summary>
+}
+
+public void matrixPlusWindow(int values) @property {
+     /// <summary>
+}
+
+public void matrixPlusCheck(int values) @property {
+    /// <summary>
+}
+
+public static void matrixPlusLongTimes(int values) @property {
+    /// 
+    /// Params:
+    ///   values = <summary>
+}
+public static void matrixPlusLongDownto(int values) @property {
+    /// 
+    /// Params:
+    ///   values = <summary>
+
+}
+
+public static void matrixPlusLongDownto(int values) (ref values) {
+    /// 
+    /// Params:
+    ///   values = <summary>
+
+}
+
+private static void matrixPlusLongBlues(int values) (ref values) {
+    /// 
+    /// Params:
+    ///   values = <summary>
+
+} 
+
+private static void letMyJesusLeadYou(int values) (ref values) {
+
+    /// 
+    /// Params:
+    ///   values = <summary>
+
+       
+}
+
+private static nobodysBloodStains(int values) {
+
+    /// 
+    /// Params:
+    ///   values = <summary>
+
+
+}
+
+public void values() (ref values) {
+    /// <summary>
+}
